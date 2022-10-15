d44e4e3db0a3f1d1ea7d9e001a262929fec483a1
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-10-15 18:21:17 +0300

    Backend <> Adding get jobs for specific company api

--- backend/linkedin-server/controllers/jobs.controller.js
+++ backend/linkedin-server/controllers/jobs.controller.js
@@ -4,21 +4,29 @@ const Job = require('../models/jobs.model');
 const postJob = async (req, res)=>{
     const {title, description, company_id, company_name, category} = req.body;
     try{
-        const job = new Job();
-        job.title = title;
-        job.description = description;
-        job.company_id = company_id;
-        job.company_name = company_name;
-        job.category = category
-        await job.save();
-        res.json(job)
+            const job = new Job();
+            job.title = title;
+            job.description = description;
+            job.company_id = company_id;
+            job.company_name = company_name;
+            job.category = category
+            await job.save();
+            res.json(job)
         }catch(err){
-        res.status(400).json({
-        message: err.message
-    })
-}
+            res.status(400).json({
+            message: err.message
+        })
+    }
 }
 
+//get all jobs for specific company
+const getJobs = async (req, res) => {
+    const id = req.params.id;
+    await Job.find({company_id:id}).then((job)=>res.send(job))
+}
+    
+
 module.exports = {
-    postJob
+    postJob,
+    getJobs
 }
