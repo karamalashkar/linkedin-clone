7f62a0aed5a9cf1b5e222be1670e597cc075cc84
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-10-15 20:27:54 +0300

    Backend <> Connecting all routes to main file

--- backend/linkedin-server/index.js
+++ backend/linkedin-server/index.js
@@ -5,6 +5,15 @@ require('./config/database.config')
 const app=express();
 app.use(express.json());
 
+const authRoutes = require('./routes/auth.routes');
+app.use('/auth', authRoutes)
+
+const usersRoutes = require('./routes/users.routes');
+app.use('/users', usersRoutes)
+
+const jobsRoutes = require('./routes/jobs.routes');
+app.use('/jobs', jobsRoutes)
+
 app.listen(process.env.PORT, (err)=>{
     if(err) throw err;
     console.log(`Server running on port ${process.env.PORT}`);
