e78c84eab15be96a149993a213d7cbd60fe7ac7e
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-10-15 16:41:37 +0300

    Backend <> Adding add company api

--- backend/linkedin-server/controllers/users.controller.js
+++ backend/linkedin-server/controllers/users.controller.js
@@ -1,5 +1,6 @@
 const User = require('../models/users.model')
 
+//update User
 const updateUser = async (req, res) => {
     const id=req.params.id
     const {image,bio,location} = req.body
@@ -11,7 +12,18 @@ const updateUser = async (req, res) => {
     .then((user)=>res.send(user))
     .catch((err)=>res.status(400).send(err))
 }
+
+//add company
+const addCompany = async (req,res)=>{
+    const id=req.params.id;
+    const user=await User.findById(id);
+    user.company=req.body
+    user.save()
+    res.json(user)
+}
+    
     
 module.exports = {
-    updateUser
+    updateUser,
+    addCompany
 }
