0d3686dd23d80b93442fcd1b269e98a59205e8b4
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-10-15 22:31:00 +0300

    Frontend <> Adding navbar for landing page

---
+++ frontend/linkedin-application/src/components/landingbar/LandingBar.js
@@ -0,0 +1,11 @@
+import "./style.css";
+
+const LandingBar = () => {
+    return (
+        <div className="navbar"> 
+            <img src="logo.png"></img>
+        </div>
+    )
+};
+
+export default LandingBar;
