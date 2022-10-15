217af59dc8de8a31beed5d5a093dbd120294c998
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-10-15 15:26:39 +0300

    Backend <> Adding login api

--- backend/linkedin-server/controllers/auth.controller.js
+++ backend/linkedin-server/controllers/auth.controller.js
@@ -1,5 +1,23 @@
 const User = require('../models/users.model');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const login = async (req, res)=>{
+    const {email, password} = req.body;
+    
+    const user = await User.findOne({email}).select("password");
+
+    if(!user) return res.json({message: "Invalid Credentials"});
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch) return res.json({message: "Invalid Credentials"});
+
+    const token = jwt.sign({email: user.email, name: user.name}, process.env.JWT_SECRET_KEY, {
+        expiresIn: '1h'
+    });
+    res.json(token)
+}
+
 
 const signup = async (req, res)=>{
     const {name, email, password,image,bio,location} = req.body;
@@ -22,5 +40,6 @@ const signup = async (req, res)=>{
 }
 
 module.exports = {
-    signup
+    signup,
+    login
 }
