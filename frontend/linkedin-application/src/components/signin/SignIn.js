2a3abbc02c30e3b9d9bf9270ef2f512895559233
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-10-16 07:01:31 +0300

    Frontend <> Adding signin form

--- frontend/linkedin-application/src/components/signin/SignIn.js
+++ frontend/linkedin-application/src/components/signin/SignIn.js
@@ -3,11 +3,11 @@ import "./style.css";
 const SignIn = () => {
     return (
         <div className="sign">
-            <h2>Sign In</h2>
-            <h4>Stay updated on your professional world</h4>
+            <h2 className="signin-title">Sign In</h2>
+            <h4 className="signin-subtitle">Stay updated on your professional world</h4>
             <input type="email" placeholder="Email" className="input"></input>
             <input type="password" placeholder="Password" className="input"></input>
-            <input type="submit" value="Join" className="input btn"></input>
+            <input type="submit" value="Sign in" className="input btn"></input>
         </div>
     )
 };
