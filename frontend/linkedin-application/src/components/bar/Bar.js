010aafe9f698a48a8ef4f3e8862ea36675d6f811
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-10-16 12:08:26 +0300

    Frontend <> Adding bar for company page

---
+++ frontend/linkedin-application/src/components/bar/Bar.js
@@ -0,0 +1,13 @@
+import './style.css';
+
+const Bar = () =>{
+    return(
+        <div className='bar'>
+            <select className='input'>
+                <option value=''>Job</option>
+            </select>
+        </div>
+    )
+}
+
+export default Bar;
