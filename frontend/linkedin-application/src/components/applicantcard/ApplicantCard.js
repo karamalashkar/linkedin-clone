562cedf22aeb0ce95a6f6b610a4e3e6369ad03ae
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-10-16 12:43:36 +0300

    Frontend <> Adding applicant card

---
+++ frontend/linkedin-application/src/components/applicantcard/ApplicantCard.js
@@ -0,0 +1,18 @@
+import './style.css';
+
+const ApplicantCard = () => {
+    return(
+    <>
+        <div className='card'>
+            <div className='applicant-info'>
+                <h1></h1>
+                <h3></h3>
+                <h4></h4>
+                <p className='applicant-bio'></p>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default ApplicantCard;
