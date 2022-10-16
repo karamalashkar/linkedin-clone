042bdcb2f5a9c701d067f88e3fa5aa138b2dc6a6
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-10-16 11:41:58 +0300

    Frontend <> Adding side bar

---
+++ frontend/linkedin-application/src/components/sidebar/SideBar.js
@@ -0,0 +1,16 @@
+import './style.css';
+
+const SideBar = () =>{
+    return(
+        <div className='sidebar'>
+            <div className='company-info'>
+                <h1></h1><br></br>
+                <h2></h2><br></br>
+                <p></p>
+                <button className='button'>Add</button>
+            </div>    
+        </div>
+    )
+}
+
+export default SideBar;
