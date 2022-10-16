8abd260285f70c4a4d02054a6b699a23354b3b7c
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-10-16 10:52:37 +0300

    Frontend <> Adding profile page

--- frontend/linkedin-application/src/components/profile/Profile.js
+++ frontend/linkedin-application/src/components/profile/Profile.js
@@ -1,16 +1,29 @@
+import React,{useState} from 'react';
 import "./style.css";
 import Button from "../button/Button";
+import Navbar from '../navbar/Navbar';
+import EditProfile from "../editprofile/EditProfile";
+import AddCompany from '../addcompany/AddCompany';
 
 const Profile = () =>{
+    const [isOpen,setIsOpen]=useState(false)
+    const [isOpenCompany,setOpen]=useState(false)
+    
     return(
+        <>
+        <Navbar></Navbar>
+        <EditProfile open={isOpen} onClose={()=>setIsOpen(false)}></EditProfile>
+        <AddCompany open={isOpenCompany} onClose={()=>setOpen(false)}></AddCompany>
+        <div className='profile-section'>
         <div className="profile">
             <div className="cover"></div>
             <img className="profile-image" src="landing.png"></img>
             <div className="buttons">
-                <Button text={'Edit'}></Button>
-                <Button text={'Company'}></Button>
-                <Button text={'Logout'}></Button>
+                <button className='button' onClick={()=>setIsOpen(true)}>Edit</button>
+                <button className='button' onClick={()=>setOpen(true)}>Company</button>
+                <button className='button'>Logout</button>
             </div>    
+            
             <div className="profile-info">
                 <h1></h1>
                 <h3></h3>
@@ -18,6 +31,8 @@ const Profile = () =>{
                 <p className="bio"></p>
             </div>
         </div>
+        </div>
+        </>
     )    
 }
 
