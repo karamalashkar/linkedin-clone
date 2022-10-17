d45781c48270255e9c3dc8bc225fba2b9057f9db
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-10-17 23:23:20 +0300

    Frontend <> Editing link to add a company

--- frontend/linkedin-application/src/hooks/addCompany.js
+++ frontend/linkedin-application/src/hooks/addCompany.js
@@ -17,36 +17,3 @@ export const add_company = async (id,token,post) => {
     
     
     
-    /*
-
-
-
-    headers:{
-//         'Authorization': 'Bearer ' + token
-//     }
-
-    
-    await axios.post(url_user+`/add_company/${id}`, {
-        headers:{
-            'Authorization': `Bearer ${token}`
-        },
-        post
-    })
-    .then(response => { 
-        console.log(response)
-        return response.data
-    })
-    .catch(error => {
-        console.log(error.response)
-    });
-    /*const res = await axios.post(url_user+`/update/${id}`,{
-        headers:{
-            'authorization': `Bearer ${token}`
-        },
-        post
-    }
-    ).
-    return res;*/
-    //console.log(post.company_category)
- 
-
