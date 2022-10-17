88e27aa3618dd5166d06edfc93b208c70b0b7606
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-10-17 23:00:52 +0300

    Frontend <> Editing edit profile form

--- frontend/linkedin-application/src/components/editprofile/EditProfile.js
+++ frontend/linkedin-application/src/components/editprofile/EditProfile.js
@@ -1,19 +1,69 @@
+import { useState } from "react";
+import { editProfile } from "../../hooks/editprofile";
 import "./style.css";
 
-const EditProfile = () =>{
+const EditProfile = ({open,onClose}) =>{
+    const id=localStorage.getItem('id');
+    const token=localStorage.getItem('token');
+
+    const [image,setImage]=useState('')
+    const [bio,setBio]=useState('')
+    const [location,setLocation]=useState('')
+
+    const onSubmit = async (e)=>{
+        e.preventDefault();
+        
+        if(!location || !bio){
+            return null
+        }
+
+        const post = {bio,location};
+        const user = await editProfile(id,token,post);
+        localStorage.setItem('location',location);
+        localStorage.setItem('bio',bio);
+    }    
+
+
+    if(!open){
+        return null
+    }
     return(
+        <form onSubmit={onSubmit}>
         <div className="edit">
             <div className="edit-item">
-                <button className="close">
+                <button className="close" onClick={onClose}>
                     x
                 </button>
                 <h1>Edit Profile</h1>
-                <input type="file" className="edit-input"></input>
-                <input type="text" className="edit-input" placeholder="Location"></input>
-                <textarea className="edit-input text" placeholder="Bio" maxlength="850"></textarea>
+                
+                <input type="file"
+                onChange={(event)=>
+                setImage(event.target.value)}
+                value={image} 
+                className="edit-input"
+                ></input>
+                
+                <input type="text"
+                onChange={(event)=>{
+                setLocation(event.target.value)}} 
+                value={location} 
+                className="edit-input" 
+                placeholder="Location"
+                ></input>
+                
+                <textarea
+                onChange={(event)=>{   
+                setBio(event.target.value)}}
+                value={bio} 
+                className="edit-input text" 
+                placeholder="Bio" 
+                maxLength="850"
+                ></textarea>
+                
                 <button className="edit-button">Edit</button>
             </div>
         </div>
+        </form>
     )    
 }
 
