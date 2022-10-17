0b2c4c588c55202c80521ef73c7a0937adb53683
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-10-17 22:56:09 +0300

    Frontend <> Adding applicant form

---
+++ frontend/linkedin-application/src/components/applicant/Applicant.js
@@ -0,0 +1,11 @@
+import './style.css';
+import ApplicantCard from '../applicantcard/ApplicantCard';
+
+const Applicant = () =>{
+    return(
+        <div className='applicant'>
+        </div>
+    )
+}
+
+export default Applicant;
